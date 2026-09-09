0fe3d50636ccec1fc6ca10cca9595d0a0c533ff7
hua <zxc020927150@gmail.com>
2026-09-09 22:14:19 +0800

    SEO

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ const theme = () => {
   const html = document.documentElement;
   const icon = document.querySelector(".data__theme i");
   const theme = localStorage.getItem("theme");
-  console.log(theme)
-  if (theme === 'dark') {
+  // 判斷localStorage儲存的使用者習慣，沒有就是淺色
+  if (theme === "dark") {
     icon.className = "fa-regular fa-sun";
     html.setAttribute("data-theme", "dark");
-    button.setAttribute('aria-label',"切換淺色模式")
-    button.setAttribute('title',"切換淺色模式")
+    button.setAttribute("aria-label", "切換淺色模式");
+    button.setAttribute("title", "切換淺色模式");
   }
+  // 會切換icon跟提示字，並記錄在localStorage
   button.addEventListener("click", () => {
     if (html.getAttribute("data-theme") === "dark") {
       icon.className = "fa-regular fa-moon";
       html.removeAttribute("data-theme");
-      button.setAttribute('aria-label',"切換深色模式")
-      button.setAttribute('title',"切換深色模式")
+      button.setAttribute("aria-label", "切換深色模式");
+      button.setAttribute("title", "切換深色模式");
       localStorage.setItem("theme", "light");
     } else {
       icon.className = "fa-regular fa-sun";
       html.setAttribute("data-theme", "dark");
-      button.setAttribute('aria-label',"切換淺色模式")
-      button.setAttribute('title',"切換淺色模式")
+      button.setAttribute("aria-label", "切換淺色模式");
+      button.setAttribute("title", "切換淺色模式");
       localStorage.setItem("theme", "dark");
     }
   });
@@ -33,17 +34,20 @@ theme();
 const navbar = () => {
   const toggle = document.querySelector(".nav__toggle");
   const menu = document.querySelector(".nav__menu");
-  const isOpen = () => {
+  const links = document.querySelectorAll(".nav__link");
+  // 點擊漢堡選單切換開關
+  toggle.addEventListener("click", () => {
     menu.classList.toggle("isOpen");
     menu.classList.contains("isOpen")
       ? toggle.setAttribute("aria-expanded", true)
       : toggle.setAttribute("aria-expanded", false);
-  };
-  toggle.addEventListener("click", () => {
-    isOpen();
   });
-  menu.addEventListener("click", () => {
-    isOpen();
+  // 點擊link會關掉選單
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("isOpen");
+      toggle.setAttribute("aria-expanded", false);
+    });
   });
 };
 navbar();
@@ -56,10 +60,11 @@ const nav_into = () => {
 
   const observer = new IntersectionObserver(
     (entries) => {
+      //篩選有進入的區塊，可能有多個，都沒進入，就不處理
       const visible = entries.filter(
-        (entry) => entry.isIntersecting, //篩選有進入的區塊，可能有多個
+        (entry) => entry.isIntersecting, 
       );
-      if (visible.length === 0) return; //都沒進入，不處理
+      if (visible.length === 0) return;
 
       //同時進入多個的話，挑「頂端最靠近 0」的那一個當作目前所在區塊 (取絕對值最小)
       const current = visible.reduce((closest, entry) =>
@@ -68,17 +73,20 @@ const nav_into = () => {
           ? entry
           : closest,
       );
+      // 取目標對象id
       const id = current.target.id;
+      
       // 更改字體大小
       const current_link = document.querySelector(
         `.nav__item a[href="#${id}"]`,
-      ); //找到正確修改對象
+      ); 
       nav__links.forEach((nav__link) => {
         nav__link.classList.remove("into");
       });
       if (current_link) {
         current_link.classList.add("into");
       }
+
       // 更改背景
       nav__items.forEach((nav__item) => {
         nav__item.classList.remove("bg");
